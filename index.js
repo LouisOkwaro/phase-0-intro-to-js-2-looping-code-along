b5297edd730da03d0c8d259766fe763866472ba0
LouisOkwaro <okwarolouis@gmail.comgit config --global user.email okwarolouis@gmail.com>
2022-12-05 18:30:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const friends = ["Nicole", "Allan", "Miles"];
+const occasion =["Baby Shower"]
+const messages=[];
+
+function writeCards(friends, occasion){
+    for(let i = 0; i < friends.length; i++) {
+        const card = (`Thank you, ${friends[i]}, for the wonderful ${occasion} gift!`);
+        messages.push(card);
+        
+    }
+    return messages;
+}
+
+
+function countDown(a){
+    while (a >= 0) {
+        console.log(a);
+        a--
+    }
+}
